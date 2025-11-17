f0fbc789b43621e044ab81c0862ea797d52eb2d4
Rory Doak <rory.doak@kestrix.io>
2025-11-17 16:16:34 +0000

    update index html for redirects

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://rodo94.github.io/",
+  base: "/",
   plugins: [
     tanstackRouter({
       target: "react",
